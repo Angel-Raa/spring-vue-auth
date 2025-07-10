9e11d3f871b3eede3cfc1b39075ad462ca49077b
Angel Aguero <angelagueror23@gmail.com>
2025-07-09 23:29:22 -0400

    feat: Agregar clases BookRequest y Response para manejar solicitudes y respuestas de libros

---
+++ server/src/main/java/io/github/angel/raa/dto/request/book/BookRequest.java
@@ -0,0 +1,37 @@
+package io.github.angel.raa.dto.request.book;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class BookRequest implements Serializable {
+    private static final long serialVersionUID = 121323548391384337L;
+    @NotBlank(message = "El título no puede estar vacío")
+    @Size(max = 100, message = "El título no puede tener más de 100 caracteres")
+    private String title;
+    @NotBlank(message = "El autor no puede estar vacío")
+    private String author;
+    @NotBlank(message = "El ISBN no puede estar vacío")
+    @Pattern(regexp = "^[0-9\\-]{10,13}$", message = "El ISBN debe tener entre 10 y 13 dígitos o guiones")
+    @Size(max = 13, message = "El ISBN no puede tener más de 13 caracteres")
+    private String isbn;
+    @NotNull(message = "El año de publicación no puede estar vacío")
+    @PastOrPresent(message = "El año de publicación no puede ser futuro")
+    private LocalDate publicationYear;
+    @NotNull(message = "El estado no puede estar vacío")
+    private Boolean available;
+
+}
---
+++ server/src/main/java/io/github/angel/raa/dto/response/Response.java
@@ -0,0 +1,59 @@
+package io.github.angel.raa.dto.response;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response<T> implements Serializable{
+    private static final long serialVersionUID = -5357573548391384337L;
+
+    public Response(String message, T data, int status, LocalDateTime timestamp, String error) {
+        this.message = message;
+        this.data = data;
+        this.status = status;
+        this.timestamp = timestamp != null ? timestamp : LocalDateTime.now();
+        this.error = error;
+    }
+    private String message;
+    private T data;
+    private int status;
+    private LocalDateTime timestamp;
+    private String error;
+
+    public Response() {
+        this.timestamp = LocalDateTime.now();
+
+    }
+
+    public static <T> Response<T> success(T data) {
+        return Response.<T>builder()
+                .status(HttpStatus.OK.value())
+                .message("Success")
+                .data(data)
+                .build();
+    }
+
+        public static <T> Response<T> error(HttpStatus status, String error) {
+        return Response.<T>builder()
+                .status(status.value())
+                .error(error)
+                .build();
+    }
+
+    public static <T> Response<T> error(HttpStatus status, String error, String message) {
+        return Response.<T>builder()
+                .status(status.value())
+                .error(error)
+                .message(message)
+                .build();
+    }
+}
